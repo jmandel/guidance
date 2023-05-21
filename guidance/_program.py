dba55f8dccf5078c2db12b4a7afd090bb1db549f
Josh Mandel <jmandel@alum.mit.edu>
2023-05-21 18:26:17 -0500

    Add update callback

--- guidance/_program.py
+++ guidance/_program.py
@@ -33,7 +33,7 @@ class Program:
     the generated output to mark where template tags used to be.
     '''
 
-    def __init__(self, text, llm=None, cache_seed=0, logprobs=None, silent='auto', async_mode=False, stream=None, caching=None, await_missing=False, **kwargs):
+    def __init__(self, text, llm=None, cache_seed=0, logprobs=None, silent='auto', async_mode=False, stream=None, caching=None, await_missing=False, update_callback=None, **kwargs):
         """ Create a new Program object from a program string.
 
         Parameters
@@ -68,6 +68,9 @@ class Program:
             for executing programs on different machines, for example shipping a program to a GPU machine
             then waiting for the results to come back for any local processing, then shipping it back to
             the GPU machine to continue execution.
+        update_callback : Callable[[str], None] (default None)
+            If set, this callback will be invoked each time an update is available for rendering. It should be a function
+            that accepts a single string parameter, which will be the updated text of the program.
         """
 
         # see if we were given a raw function instead of a string template
@@ -80,7 +83,7 @@ class Program:
             fname = _utils.find_func_name(text, kwargs)
             kwargs[fname] = text
             text = "{{set (%s%s)}}" % (fname, args)
-        
+
         # save the given parameters
         self._text = text
         self.llm = llm or getattr(guidance, "llm", None)
@@ -93,7 +96,8 @@ class Program:
         self.await_missing = await_missing
         if self.silent == "auto":
             self.silent = not _utils.is_interactive()
-        
+        self.update_callback = update_callback
+
         # set our variables
         self._variables = {}
         self._variables.update(_built_ins)
@@ -101,10 +105,10 @@ class Program:
             "llm": llm
         })
         self._variables.update(kwargs)
-        
+
         # set internal state variables
         self._id = str(uuid.uuid4())
-        
+
         self._comm = None # front end communication object
         self._executor = None # the ProgramExecutor object that is running the program
         self._last_display_update = 0 # the last time we updated the display (used for throttling updates)
@@ -126,17 +130,17 @@ class Program:
             self._ipython = get_ipython()
         except:
             self._ipython = None
-        
+
         # if we are echoing in ipython we assume we can display html
         if self._ipython and not self.silent:
             self._displaying_html = True
-    
+
     def __repr__(self):
         return self.text
-    
+
     def __getitem__(self, key):
         return self._variables[key]
-    
+
     def _interface_event(self, msg):
         """ Handle an event from the front end.
         """
@@ -151,15 +155,15 @@ class Program:
         """
 
         log.debug(f"displaying program in _ipython_display_ with self._comm={self._comm}, self.id={self._id}")
-        
+
         # mark that we are displaying (and so future execution updates should be displayed)
         self._displaying = True
         self._displaying_html = True
-        
+
         # build and display the html
         html = self._build_html(self.marked_text)
         self._display_html(html)
-        
+
 
     async def _await_finish_execute(self):
         """ Used by self.__await__ to wait for the program to complete.
@@ -169,7 +173,7 @@ class Program:
 
     def __await__(self):
         return self._await_finish_execute().__await__()
-        
+
     def __call__(self, **kwargs):
         """ Execute this program with the given variable values and return a new executed/executing program.
 
@@ -203,7 +207,7 @@ class Program:
 
         # create an executor for the new program (this also marks the program as executing)
         new_program._executor = ProgramExecutor(new_program)
-        
+
         # if we are in async mode, schedule the program in the current event loop
         if new_program.async_mode:
             loop = asyncio.get_event_loop()
@@ -220,11 +224,11 @@ class Program:
                 nest_asyncio.apply(other_loop)
             except RuntimeError:
                 pass
-            
+
             loop = asyncio.new_event_loop()
             loop.create_task(new_program.update_display.run()) # start the display updater
             loop.run_until_complete(new_program.execute())
-    
+
         return new_program
 
     def _update_display(self, last=False):
@@ -241,10 +245,13 @@ class Program:
 
         log.debug(f"Updating display (last={last}, self._displaying={self._displaying}, self._comm={self._comm})")
 
+        if self.update_callback:
+            self.update_callback(self.marked_text)
+            
         # this is always called during execution, and we only want to update the display if we are displaying
         if not self._displaying:
             return
-        
+
         # debounce the display updates
         # now = time.time()
         # log.debug(now - self._last_display_update)
@@ -252,20 +259,20 @@ class Program:
         # if last or (now - self._last_display_update > debounce_delay):
         if self._displaying_html:
             out = self._build_html(self.marked_text)
-            
+
             # clear the send queue if this is the last update
             if last and self._comm:
                 self._comm.clear_send_queue()
-            
+
             # send an update to the front end client if we have one...
-            # TODO: we would like to call `display` for the last update so NB saving works, but see https://github.com/microsoft/vscode-jupyter/issues/13243 
+            # TODO: we would like to call `display` for the last update so NB saving works, but see https://github.com/microsoft/vscode-jupyter/issues/13243
             if self._displayed and self._comm and self._comm.is_open: #(not last or self._comm.is_open):
                 log.debug(f"Updating display send message to front end")
                 # log.debug(out)
                 self._comm.send({"replace": out})
                 if last:
                     self._comm.send({"event": "complete"})
-            
+
             # ...otherwise dump the client to the front end
             else:
                 log.debug(f"Updating display dump to front end")
@@ -274,7 +281,7 @@ class Program:
                     clear_output(wait=True) # TODO: should use wait=True but that doesn't work in VSCode until after the April 2023 release
 
                 self._display_html(out)
-        
+
         self._last_display_update = time.time()
 
     def _display_html(self, html):
@@ -283,7 +290,7 @@ class Program:
         # create the comm object if we don't have one
         if self._comm is None:
             self._comm = _utils.JupyterComm(self._id, self._ipython, self._interface_event)
-        
+
         # dump the html to the front end
         html = f"""<div id="guidance-stop-button-{self._id}" style="cursor: pointer; margin: 0px; display: none; float: right; padding: 3px; border-radius: 4px 4px 4px 4px; border: 0px solid rgba(127, 127, 127, 1); padding-left: 10px; padding-right: 10px; font-size: 13px; background-color: rgba(127, 127, 127, 0.25);">Stop program</div><div id="guidance-content-{self._id}">{html}</div>
 <script type="text/javascript">{js_data}; window._guidanceDisplay("{self._id}");</script>"""
@@ -299,11 +306,11 @@ class Program:
         """
 
         log.debug(f"Executing program (self.async_mode={self.async_mode}, self.silent={self.silent}, self._displaying_html={self._displaying_html})")
-        
+
         # if we are already displaying html, we need to yield to the event loop so the jupyter comm can initialize
         if self._displaying_html:
             await asyncio.sleep(0)
-        
+
         # run the program and capture the output
         if self.llm is None:
             await self._executor.run(None)
@@ -321,16 +328,16 @@ class Program:
 
         # fire an event noting that execution is complete (this will release any await calls waiting on the program)
         self._execute_complete.set()
-    
+
     def __getitem__(self, key):
         return self._variables[key]
-    
+
     def __contains__(self, key):
         return key in self._variables
-    
+
     def __delitem__(self, key):
         del self._variables[key]
-    
+
     def variables(self, built_ins=False):
         """ Returns a dictionary of the variables in the program.
 
@@ -340,19 +347,19 @@ class Program:
             If True, built-in variables will be included in the returned dictionary.
         """
         return {k: v for k,v in self._variables.items() if built_ins or not (k in _built_ins and callable(_built_ins[k]))}
-    
+
     @property
     def text(self):
         # strip out the markers for the unformatted output
         return _utils.strip_markers(self.marked_text)
-    
+
     @property
     def marked_text(self):
         if self._executor is not None:
             return self._executor.prefix
         else:
             return self._text
-    
+
     def _build_html(self, text, last=False):
         output = text
 
@@ -362,7 +369,7 @@ class Program:
         def start_generate_or_select(x):
             no_echo = "echo=False" in x.group(1)
             alpha = 1.0 if no_echo else 1.0
-            
+
             # script that toggles the viisibility of the next element
             click_script = 'var e = this.nextElementSibling; if (e.style.display == "inline") { e.style.display = "none"; this.style.borderRight = "1px solid rgba(0, 165, 0, 0.25)"; } else { e.style.display = "inline"; this.style.borderRight = "0px";}'
 
@@ -372,13 +379,13 @@ class Program:
             else:
                 out = "<span style='background-color: rgba(0, 165, 0, 0.25); opacity: {}; display: inline;' title='{}'>".format(alpha, undo_html_encode(x.group(1)))
             return out
-        
+
         def start_each(x):
             no_echo = "echo=False" in x.group(1)
             alpha = 0.5 if no_echo else 1.0
             color = "rgba(165, 165, 165, 0.1)" #if "geneach" not in x.group(1) else "rgba(0, 165, 0, 0.1)"
             return "<span style='opacity: {}; display: inline; background-color: {};' title='{}'>".format(alpha, color, undo_html_encode(x.group(1)))
-        
+
         def start_block(x):
             escaped_tag = undo_html_encode(x.group(1))
             if "hidden=True" in escaped_tag:
@@ -386,7 +393,7 @@ class Program:
             else:
                 display = "inline"
             return f"<span style='background-color: rgba(165, 165, 165, 0.1); display: {display};' title='{escaped_tag}'>"
-        
+
         def role_box(x):
             # name = x.group(3).lower() # standardize to lowercase for display
             # content = x.group(4)
@@ -394,11 +401,11 @@ class Program:
             content = x.group(3)
             tag_text = undo_html_encode(x.group(2))
             role_name = x.group(1)
-            
+
             # if we have a generic role tag then the role name is an attribute
             if role_name == "role":
                 role_name = re.search(r"name=([^ ]*)", tag_text).group(1)
-            
+
             start_pattern = html.escape(self.llm.role_start(role_name)).replace("|", r"\|")
             end_pattern = html.escape(self.llm.role_end(role_name)).replace("|", r"\|")
 
@@ -406,7 +413,7 @@ class Program:
             content = re.sub("^" + start_pattern, "", content, flags=re.DOTALL)
             content = re.sub(end_pattern + "$", "", content, flags=re.DOTALL)
 
-            
+
             # one div that contains two divs, where the left of the two inner divs has a fixed width of 100px
             # """<div style='display: flex;'>
             #     <div style='width: 100px; border-right: 1px solid rgba(127, 127, 127, 0.2); padding-right: 5px; margin-right: 5px;'>{name}</div>
@@ -421,7 +428,7 @@ class Program:
         display_out = html.escape(output)
         # log.debug(display_out)
 
-        
+
         # start_pattern = html.escape(self.llm.role_start("(.*?)")).replace("|", r"\|")
         # end_pattern = html.escape(self.llm.role_end("(.*?)")).replace("|", r"\|")
         # display_out = re.sub(r"[\s]+({{!--.*?--}})?"+start_pattern, r"\1"+start_pattern.replace("(.*?)", r"\1").replace(r"\|", "|"), display_out, flags=re.DOTALL)
@@ -430,13 +437,13 @@ class Program:
 
         # strip out hidden blocks (might want to make a better UI for this at some point)
         display_out = re.sub(r"{{!--GMARKER_START[^}]*--}}{{!--GHIDDEN:(.*?)--}}{{!--GMARKER_END[^}]*--}}", "", display_out, flags=re.DOTALL)
-        
+
         # if we have role markers, we wrap them in special formatting
         if re.search(r"{{!--GMARKER_START_(role|system|user|assistant)", display_out) is not None:
 
             # start_pattern = html.escape(self.llm.role_start("assistant")).replace("|", r"\|").replace(r"assistant", r"([^\n]*)").replace(r"ASSISTANT", r"([^\n]*)")
             # end_pattern = html.escape(self.llm.role_end("assistant")).replace("|", r"\|").replace(r"assistant", r"([^\n]*)").replace(r"ASSISTANT", r"([^\n]*)")
-            
+
             # strip whitespace before role markers
             display_out = re.sub(r"\s*{{!--GMARKER_START_(role|system|user|assistant)\$(.*?)--}}", r"{{!--GMARKER_START_\1$\2--}}", display_out, flags=re.DOTALL)
 
@@ -449,12 +456,12 @@ class Program:
 
             # wrap unfinished role markers in nice formatting
             display_out = re.sub(r"{{!--GMARKER_START_(role|system|user|assistant)\$(.*?)--}}" + "(.*)", role_box, display_out, flags=re.DOTALL)
-        
+
         display_out = re.sub(r"(\{\{generate.*?\}\})", r"<span style='background-color: rgba(0, 165, 0, 0.25);'>\1</span>", display_out, flags=re.DOTALL)
         display_out = re.sub(r"(\{\{#select\{\{/select.*?\}\})", r"<span style='background-color: rgba(0, 165, 0, 0.25);'>\1</span>", display_out, flags=re.DOTALL)
         display_out = re.sub(r"(\{\{#each [^'\"].*?\{\{/each.*?\}\})", r"<span style='background-color: rgba(0, 138.56128016, 250.76166089, 0.25);'>\1</span>", display_out, flags=re.DOTALL)
         display_out = re.sub(r"(\{\{(?!\!)(?!generate)(?!#select)(?!#each)(?!/each)(?!/select).*?\}\})", r"<span style='background-color: rgba(0, 138.56128016, 250.76166089, 0.25);'>\1</span>", display_out, flags=re.DOTALL)
-                
+
 
         # format the generate command results
         display_out = re.sub(r"{{!--GMARKER_START_gen\$([^\$]*)\$--}}", start_generate_or_select, display_out)
@@ -516,7 +523,7 @@ cycle_IDVAL(this);'''.replace("IDVAL", id).replace("TOTALCOUNT", str(total_count
 
         # format the geneach command results
         display_out = re.sub(r"{{!--GMARKER_START_geneach\$([^\$]*)\$--}}", start_each, display_out)
-        
+
         # format the set command results
         display_out = re.sub(r"{{!--GMARKER_set\$([^\$]*)\$--}}", r"<div style='background-color: rgba(165, 165, 165, 0); border-radius: 4px 4px 4px 4px; border: 1px solid rgba(165, 165, 165, 1); border-left: 2px solid rgba(165, 165, 165, 1); border-right: 2px solid rgba(165, 165, 165, 1); padding-left: 0px; padding-right: 3px; color: rgb(165, 165, 165, 1.0); display: inline; font-weight: normal; overflow: hidden;'><div style='display: inline; background: rgba(165, 165, 165, 1); padding-right: 5px; padding-left: 4px; margin-right: 3px; color: #fff'>set</div>\1</div>", display_out)
         display_out = re.sub(r"{{!--GMARKER_START_set\$([^\$]*)\$--}}", lambda x: "<span style='display: inline;' title='{}'>".format(undo_html_encode(x.group(1))), display_out)
@@ -529,7 +536,7 @@ cycle_IDVAL(this);'''.replace("IDVAL", id).replace("TOTALCOUNT", str(total_count
         display_out = re.sub(r"{{!--GMARKER_START_block\$([^\$]*)\$--}}", start_block, display_out)
         display_out = re.sub(r"{{!--GMARKER_START_([^\$]*)\$([^\$]*)\$--}}", lambda x: "<span style='background-color: rgba(0, 138.56128016, 250.76166089, 0.25); display: inline;' title='{}'>".format(undo_html_encode(x.group(2))), display_out)
         display_out = re.sub(r"{{!--GMARKER_END_([^\$]*)\$\$--}}", "</span>", display_out)
-        
+
         # display_out = re.sub(' and (?=.* and )', ', ', display_out)
 
         # strip out comments
@@ -584,7 +591,7 @@ class DisplayThrottler():
         self.throttle_limit = throttle_limit
         self._done = False
         self.last_time = 0
-    
+
     async def run(self):
         self._data_event = asyncio.Event()
         self._done_event = asyncio.Event()
